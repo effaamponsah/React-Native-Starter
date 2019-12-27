1033e76413b5b4294d7b242be318cd322a818c64
Dennis Effa Amponsah <kamponsah.amponsah@gmail.com>
2019-12-27 10:38:22 +0000

    added travis.yml fiel

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ import {
   DebugInstructions,
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
-const App: () => React$Node = () => {
+const App = () => {
   return (
     <>
       <StatusBar barStyle="dark-content" />
